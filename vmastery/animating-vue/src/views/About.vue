a64008d29eb0b6f0b11a8b2f33198f4e353e63bf
bink3060 <bink3060@gmail.com>
2020-06-15 02:21:34 +0200

    Intermediate path -> Animation

---
+++ vmastery/animating-vue/src/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="newContact" placeholder="Name" type="text" />
+    <button @click="addContact">Add Contact</button>
+    <button @click="sortContact">Sort</button>
+
+    <transition-group name="slide-up" tag="ul" appear>
+      <li v-for="contact in contacts" :key="contact">{{ contact }}</li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newContact: "",
+      contacts: ["Beau Thabeast", "Cindy Rella", "Alice Wunderlind"]
+    };
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact);
+      this.newContact = "";
+    },
+    sortContact() {
+      this.contacts.sort();
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
