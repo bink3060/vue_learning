ef6952c03220a774545b930dd524d514eec970fd
bink3060 <bink3060@gmail.com>
2020-06-18 21:53:42 +0200

    forms vuetify

---
+++ vmastery/beautify-with-vuetify/src/views/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Signup Page</h1>
+        <v-form>
+          <v-text-field label="Email" type="email"></v-text-field>
+          <v-autocomplete :items="browsers" label="Which browser do you use ?"></v-autocomplete>
+          <v-file-input label="Attach profil picture"></v-file-input>
+          <v-text-field label="birthday" v-model="birthday" readonly></v-text-field>
+          <v-date-picker v-model="birthday"></v-date-picker>
+          <v-checkbox label="Agree terms & conditions"></v-checkbox>
+          <v-btn type="submit" color="primary">Submit</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      browsers: ["Chrome", "Firefox", "safari", "Opera", "Brave"],
+      birthday: ""
+    };
+  }
+};
+</script>
+
+<style>
+</style>
